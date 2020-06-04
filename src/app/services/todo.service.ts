48418d3c2afbb862570d2aa5682fba9c0d341cd6
Nabil Far <nabilfar.info@gmail.com>
2020-06-04 11:40:52 +0200

    practicing angular

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+import { Todo } from '../models/Todo'
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=5';
+
+  constructor(private http: HttpClient) { }
+
+  // getTodos
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`)
+  }
+
+
+  // delete todo
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.delete<Todo>(url, httpOptions)
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
+  }
+
+
+  // toggle completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.put(url, todo, httpOptions)
+  }
+
+
+}
